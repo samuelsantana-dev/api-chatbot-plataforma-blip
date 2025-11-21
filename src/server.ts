4c8d8cc6faddea7eb9496980610858978b0d5dd3
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-11-21 12:28:01 -0300

    new files

--- src/server.ts
+++ src/server.ts
@@ -4,15 +4,15 @@ import cors from "cors";
 import axios from "axios";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-const CHATBOT_KEY = process.env.CHATBOT_KEY || "";
+const PORT = process.env.PORT || 3001;
+const API_CLIMA_KEY = process.env.API_CLIMA_KEY || "";
 
 app.use(cors());
 
-app.get("/chatbot-blip/", async (req, res) => {
+app.get("/chatbot-blip", async (req, res) => {
     try {
-        const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Aurora&appid=${CHATBOT_KEY}&units=metric&lang=pt_br`) 
-        console.log(data)
+        const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Aurora&appid=${API_CLIMA_KEY}&units=metric&lang=pt_br`) 
+        console.log('data', data);
         return res.json({
             event_date: 'EVENT_DATE',
             temperature: 'data.main.temp',
@@ -21,5 +21,11 @@ app.get("/chatbot-blip/", async (req, res) => {
 
     }
 })
+
+app.get("/", (req, res) => {
+    res.json({ 
+        message: "API Chatbot BLIP funcionando!",
+    });
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
