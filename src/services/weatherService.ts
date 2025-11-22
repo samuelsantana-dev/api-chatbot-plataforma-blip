21b2a85cd2e0dc18e0617e886ce5691b0a1aa3b7
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-11-22 12:46:18 -0300

    Nova env com url

--- src/services/weatherService.ts
+++ src/services/weatherService.ts
@@ -22,37 +22,3 @@ export async function getWeather(city: string) {
         raw: data
     };
 }
-
-// export async function getWeather(city: string) {
-//     console.log("🔍 Mock - Simulando dados do clima...");
-//     const WEATHER_KEY = process.env.WEATHER_KEY;
-
-//     if (!WEATHER_KEY) throw new Error("WEATHER_KEY não está definida no .env");
-
-//     //   const { data } = await axios.get(
-//     //     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${WEATHER_KEY}&units=metric&lang=pt_br`
-//     //   );
-
-//     const mockWeatherData = {
-//         event_date: new Date().toISOString(),
-//         temperature: 28.5,
-//         city: "São Paulo",
-//         humidity: 65,
-//         description: "céu limpo",
-//         feels_like: 30.2,
-//         pressure: 1013,
-//         wind_speed: 3.1,
-//         mock: true // Para identificar que são dados mockados
-//     };
-
-//     console.log('✅ Dados mockados enviados:', mockWeatherData);
-
-//     return mockWeatherData;
-
-//     //   return {
-//     //     situacao: data.weather[0].description,
-//     //     temperatura: data.main.temp,
-//     //     minima: data.main.temp_min,
-//     //     maxima: data.main.temp_max,
-//     //   };
-// }
